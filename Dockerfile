4bc108e8a8e4c19fe34b369e8b50ea7e0266bdab
Taufek Johar <taufek@gmail.com>
2024-02-16 00:55:22 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Container image that runs your code
+FROM ruby:2.7.5-alpine
+
+LABEL maintainer "Taufek Johar<taufek@caterspot.com>"
+
+RUN apk add --no-cache --upgrade git openssh build-base aws-cli jq nodejs npm yarn bash
+
+COPY entrypoint.sh /entrypoint.sh
+
+RUN chmod +x /entrypoint.sh
+
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -e
+
+echo "Setup variables ..."
+
+APP=$1
+AWS_ACCESS_KEY_ID=$2
+AWS_SECRET_ACCESS_KEY=$3
+AWS_ACCOUNT=$4
+AWS_DEFAULT_REGION=$5
+GITHUB_KEY=$6
+ENVIRONMENT=$7
+VERSION=$8
+INFRA_REPO=$9
+
+echo "Setting up ssh ..."
+
+if [ ! -d /root/.ssh/ ]; then
+   mkdir -p /root/.ssh/
+fi
+
+echo '' > /root/.ssh/known_hosts
+
+ssh-keygen -R github.com
+ssh-keyscan -H github.com >> /root/.ssh/known_hosts
+
+echo "${GITHUB_KEY}" > /root/.ssh/id_rsa
+chmod 0500 /root/.ssh/id_rsa
+
+echo "Setting up ssh agent ..."
+
+eval `ssh-agent -s`
+ssh-add /root/.ssh/id_rsa
+
+echo "Cloning Infra ... "
+git clone -b master --single-branch ${INFRA_REPO} caterspot_infra
+cd caterspot_infra/cdk/caterspot
+
+echo "Installing dependencies ..."
+yarn install
+
+echo "Deploying ${APP} to ${ENVIRONMENT} ..."
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
+
+scripts/$APP/deploy.sh $ENVIRONMENT $VERSION
+
+echo 'ok'
